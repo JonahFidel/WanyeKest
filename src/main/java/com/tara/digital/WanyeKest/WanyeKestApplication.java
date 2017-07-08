a19e1d6e64370a3a82fb667ce7387948954a80d2
jlems <jlemus@bu.edu>
2017-07-07 20:24:44 -0400

    initial spring boot project

---
+++ src/main/java/com/tara/digital/WanyeKest/WanyeKestApplication.java
@@ -0,0 +1,12 @@
+package com.tara.digital.WanyeKest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WanyeKestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WanyeKestApplication.class, args);
+	}
+}
---
+++ src/test/java/com/tara/digital/WanyeKest/WanyeKestApplicationTests.java
@@ -0,0 +1,16 @@
+package com.tara.digital.WanyeKest;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WanyeKestApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
